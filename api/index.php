c7ba1f5df608cc7db2d56d2c8f36c7f89881f1a5
dzgreeno <green17algeria@gmail.com>
2026-04-04 13:06:02 +0100

    Fix Vercel bootstrap/cache read-only filesystem errors

--- api/index.php
+++ api/index.php
@@ -1,4 +1,16 @@
 <?php
 
+// Vercel read-only filesystem workaround
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
