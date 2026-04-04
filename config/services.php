5ab4e0c8136877c44d36818940a5033a3afdce79
dzgreeno <green17algeria@gmail.com>
2026-04-04 03:44:00 +0100

    Fix: Remove duplicate FirebaseService class from config/services.php to resolve Vercel deployment crash

--- config/services.php
+++ config/services.php
@@ -1,31 +1,33 @@
 <?php
 
-namespace App\Services;
+return [
 
-use Kreait\Firebase\Factory;
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
 
-class FirebaseService
-{
-    protected $auth;
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
 
-    public function __construct()
-    {
-        $factory = (new Factory)
-            ->withServiceAccount(base_path('storage/app/firebase.json'));
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
 
-        $this->auth = $factory->createAuth();
-    }
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
 
-    // جلب جميع المستخدمين
-    public function getAllUsers()
-    {
-        $users = [];
-        $list = $this->auth->listUsers();
-
-        foreach ($list as $user) {
-            $users[] = $user;
-        }
-
-        return $users;
-    }
-}
+];
